0f0bb6f5dd79ddb5a2466ffec72304d8ed67ba4a
Alex Shopiak <alshop2004@gmail.com>
2023-11-26 13:56:52 +0000

    Update data type

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
         short[] indices = Inquirer.inquire();
 
-        float result = Calculator.calculate(indices);
+        double result = Calculator.calculate(indices);
 
         System.out.println("Result: " + result);
     }
